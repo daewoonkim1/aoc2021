0609b4c7a5fcba4dec94ebf1212d72f0dc5866b5
daewoonkim1 <90228051+daewoonkim1@users.noreply.github.com>
2021-12-02 00:43:07 -0500

    Add files via upload

---
+++ 2/2a.ts
@@ -0,0 +1,24 @@
+import * as fs from 'fs';
+
+const input = Buffer.from(fs.readFileSync('input.txt')).toString().split("\n");
+
+let x = 0, y=0;
+
+for (const dir of input){
+    const dirSpl = dir.split(" ");
+    if (dir.indexOf("forward")!==-1){
+        x+=Number.parseInt(dirSpl[1]);
+        console.log(`forward ` + x);
+    }
+    else if (dir.indexOf("down")!==-1){
+        y+=Number.parseInt(dirSpl[1]);
+        console.log(`down ` + y);
+    }
+    else if (dir.indexOf("up")!==-1){
+        y-=Number.parseInt(dirSpl[1]);
+        console.log(`up `+ y);
+    }
+}
+console.log(x);
+console.log(y);
+console.log(x*y);
