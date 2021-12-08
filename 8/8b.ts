b7bc20fdac9cab1385971292e56fcab9ef7e9ce1
daewoonkim1 <90228051+daewoonkim1@users.noreply.github.com>
2021-12-08 11:39:11 -0500

    Update 8b.ts

--- 8/8b.ts
+++ 8/8b.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 
-const input = Buffer.from(fs.readFileSync('inputs.txt')).toString().split("\n");
+const input = Buffer.from(fs.readFileSync('input.txt')).toString().split("\n");
 
 class pars {
     public sigpat:string[];
@@ -15,19 +15,6 @@ for (const inp of input){
     rules.push(new pars (inp.split(" | ")[0].split(" "), inp.split(" | ")[1].split(" ")))
 }
 
-const zero = 'abcdeg'
-const one = 'ab' //One is two letters
-const two = 'acdfg'
-const three = 'abcdf'
-const four = 'abef' //Four is 4
-const five =  'bcdef'
-const six = 'bcdefg'
-const seven = 'abd' //Seven requires three
-const eight = 'abcdefg' // Eight requires seven
-const nine = 'abcdef' //
-
-//Map the parts onto 1d array?
-
 function sortLetters (letters:string){
     return Array.from(letters).sort().reduce((prev,curr) => prev+=curr)
 }
@@ -39,34 +26,59 @@ function letterDiff (worda:string, wordb:string){
 function deecode (tendigits: string[]):string[]{
     let builder:string[] = Array(10);
 
+    let lengthfives:string[] = []
+    let lengthsixes:string[] = []
+
+    let onetoArr:string[] = []
+    let fourtoArr:string[] = []
+
     tendigits.forEach (dig => {
         if (dig.length === 2){
             builder[1]=sortLetters(dig);
+            onetoArr=Array.from(builder[1])
+        }
+        else if (dig.length === 4){
+            builder[4] = sortLetters(dig)
+            fourtoArr = Array.from(builder[4])
         }
         else if (dig.length === 3){
             builder[7]= sortLetters(dig)
         }
-        else if (dig.length === 4){
-            builder[4] = sortLetters(dig)
-        }
         else if (dig.length === 7){
             builder[8]= sortLetters(dig)
         }
+        else if (dig.length === 5){
+            lengthfives.push(sortLetters(dig));
+        }
+        else if (dig.length === 6){
+            lengthsixes.push(sortLetters(dig));
+        }
     })
 
-    const tophat = letterDiff(builder[7], builder[7]);
-    builder[6] = sortLetters(letterDiff(builder[1],builder[8]).reduceRight((prev,curr)=>prev+=curr));
-    //TODO: Figure out 0,2,3,5,9, getting late
+    const topleftcenter = letterDiff(builder[4],builder[1]);
+    builder[3] = sortLetters(lengthfives.filter(x => Array.from(x).indexOf(onetoArr[0])!==-1 && Array.from(x).indexOf(onetoArr[1])!==-1)[0]);
+    builder[5] = sortLetters(lengthfives.filter(x => Array.from(x).indexOf(topleftcenter[0])!==-1 && Array.from(x).indexOf(topleftcenter[1])!==-1)[0]);
+    builder[2] = sortLetters(lengthfives.filter(x => x!== builder[3] && x!==builder[5])[0]);
 
+    builder[9] = sortLetters(lengthsixes.filter(x => Array.from(x).indexOf(fourtoArr[0])!==-1 && Array.from(x).indexOf(fourtoArr[1])!==-1 && Array.from(x).indexOf(fourtoArr[2])!==-1 && Array.from(x).indexOf(fourtoArr[3])!==-1)[0])
+    lengthsixes = lengthsixes.filter(x => x!==builder[9])
+    builder[0] = sortLetters(lengthsixes.filter(x => Array.from(x).indexOf(onetoArr[0])!==-1 && Array.from(x).indexOf(onetoArr[1])!==-1)[0]);
+    lengthsixes = lengthsixes.filter(x => x!==builder[0])
+    builder[6] = lengthsixes[0]
     return builder;
 }
 
 let sum = 0;
 rules.forEach (x => {
-    let rules:number[] = []
-    x.sigpat.forEach(pat => {
-        const decode:string[] = deecode(pat.split(" "));
+    const decode:string [] = deecode(x.sigpat);
+
+    let letterbuilder=""
+    x.fourdigval.forEach(four=> {
+        letterbuilder+=decode.indexOf(sortLetters(four)) +""
     })
+
+    sum+=Number.parseInt(letterbuilder);
 })
+
 console.log(sum);
 console.log("");
